efd028f193491eac08a1219e5e53935429ad42d6
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2026-06-15 08:54:53 +0300

    refactor(logger): remove obsolete eslint-disable comments for no-restricted-syntax
    ESLint rule no-restricted-syntax no longer applies to console methods; inline disable comments are dead code.

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -7,20 +7,16 @@ export interface Logger {
 
 export const ConsoleLogger: Logger = {
   info: (msg, ctx) => {
-    // eslint-disable-next-line no-restricted-syntax
     console.log(JSON.stringify({ level: 'info', message: msg, ...ctx }));
   },
   error: (msg, err, ctx) => {
-    // eslint-disable-next-line no-restricted-syntax
     console.error(JSON.stringify({ level: 'error', message: msg, error: err instanceof Error ? err.message : err, ...ctx }));
   },
   warn: (msg, ctx) => {
-    // eslint-disable-next-line no-restricted-syntax
     console.warn(JSON.stringify({ level: 'warn', message: msg, ...ctx }));
   },
   debug: (msg, ctx) => {
     if (process.env.NODE_ENV !== 'production') {
-      // eslint-disable-next-line no-restricted-syntax
       console.debug(JSON.stringify({ level: 'debug', message: msg, ...ctx }));
     }
   }
